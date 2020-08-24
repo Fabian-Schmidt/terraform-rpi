fcf4b92ecbc0195b91bfa101e0e533e537a51cb7
Fabian-Schmidt <Fabian-Schmidt@users.noreply.github.com>
2020-08-24 11:24:27 +1000

    fix shutdown script

--- Putty_ShutdownAll.bat
+++ Putty_ShutdownAll.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 echo Raspi4-01
-plink -batch -ssh pi@192.168.10.22 -i ssh.ppk sudo nohup sh -c 'sleep 2; shutdown -r now' &
+plink -batch -ssh pi@192.168.10.22 -i ssh.ppk sudo nohup sh -c 'sleep 2; shutdown now' &
 echo Raspi3-01
-plink -batch -ssh pi@192.168.10.21 -i ssh.ppk sudo nohup sh -c 'sleep 2; shutdown -r now' &
+plink -batch -ssh pi@192.168.10.21 -i ssh.ppk sudo nohup sh -c 'sleep 2; shutdown now' &
 echo Raspi2-01
-plink -batch -ssh pi@192.168.10.20 -i ssh.ppk sudo nohup sh -c 'sleep 2; shutdown -r now' &
+plink -batch -ssh pi@192.168.10.20 -i ssh.ppk sudo nohup sh -c 'sleep 2; shutdown now' &
 echo OpenWrt-Slate
 plink -batch -ssh root@192.168.10.1 -i ssh.ppk sh -c 'sleep 2; halt' &
 pause
